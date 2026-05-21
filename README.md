d9034a0a789825d912d89121a6ed106fead448bc
Tihon Lvov <tihon@lvov.uk>
2026-05-21 10:55:30 +0100

    Some all-round improvements

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Please refer to https://www.sqlalchemy.org/ for more information.
 1. Set up a virtual environment and use pip to install all requirements
 2. First review the schema SQL file to understand the database structure
 3. Then create a new PSQL database named "test_sqlalchemy"
-4. Then review basic_sqlalchemy.py
-5. Then review advanced_sqlalchemy.py
+4. Create a .env file containing USER={Your postgres username} and PASSWORD={Your postgres password}
+5. Then review basic_sqlalchemy.ipynb
+6. Then review advanced_sqlalchemy.ipynb
--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-DROP TABLE student_course;
-DROP TABLE student;
 DROP TABLE course;
 DROP TABLE lecturer;
 
@@ -12,15 +10,4 @@ CREATE TABLE course (
     course_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     course_title VARCHAR(30) UNIQUE NOT NULL,
     lecturer_id INT REFERENCES lecturer(lecturer_id)
-);
-
-CREATE TABLE student (
-    student_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    student_name VARCHAR(30)
-);
-
-CREATE TABLE student_course (
-    student_course_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    course_id INT REFERENCES course(course_id),
-    student_id INT REFERENCES student(student_id)
 );
